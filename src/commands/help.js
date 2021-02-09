cb234eb9b114b39eb5f932d9eba029258a0aa60c
karla <a@a.com>
2021-02-09 22:56:41 +0100

    Name randomizer added with database access.

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,8 +5,9 @@ module.exports = async (message) => {
         'LeP <:globe_hp:793443892367982603>, AsP <:globe_mana:793443931572011039>, Gold <:2992_Terraria_GoldCoin:793443084368216075>, Silber <:2436_Terraria_SilverCoin:793443120951590942>, Kupfer <:8717_Terraria_CopperCoin:793442344178548737>, Verfall :wilted_rose:\n' +
         '**$add** - Fügt den spezifizierten Wert zu Deinem User hinzu.\n' +
         '**$remove** - Entfernt den spezifizierten Wert von Deinem User.\n' +
-        '**$random** - Führt einen Zufallswurf auf einer spezifizierten Liste aus.\n' +
-        'Charakter, Wetter, Waffe, Krankheit, Witz, Name ( -> Mittelreich, Horasreich -> m, w) \n' +
+        '**$random** - Führt einen Zufallswurf auf einer spezifizierten Liste aus:\n' +
+        'Charakter, Wetter, Waffe, Krankheit, Witz, Name ( -> Albernia, Gareth, Horasreich, Weiden, Bornland, Tulamidisch' +
+        ' -> männlich, weiblich) \n' +
         '**$help** - Zeigt diese Liste von Befehlen an.\n'
     )
 
--- src/commands/random.js
+++ src/commands/random.js
@@ -1,6 +1,6 @@
 const randomizer = require('./config/randomizer');
 
-module.exports = async (message, args) => {
+module.exports = async (message, args, db) => {
 
     if (args[0].toUpperCase().startsWith('WE')) {
         let item = randomizer.getWeather();
@@ -17,27 +17,77 @@ module.exports = async (message, args) => {
         message.channel.send('Du bist an **' + item + '** erkrankt.');
     } else if (args[0].toUpperCase().startsWith('WI')) {
         let item = randomizer.getJokes();
-        message.channel.send(item + ' :rofl:');
+        message.channel.send(item);
     } else if (args[0].toUpperCase().startsWith('N')) {
-        if (typeof args[1] !== 'undefined' && args[1].toUpperCase().startsWith('M')) {
-            if (args[2].toUpperCase().startsWith('M')) {
-                message.channel.send(randomizer.getRandomItem(randomizer.constants.maleFirstNamesMR) + ' ' + randomizer.getRandomItem(randomizer.constants.familyNamesMR));
-            } else if (args[2].toUpperCase().startsWith('W')) {
-                message.channel.send(randomizer.getRandomItem(randomizer.constants.femaleFirstNamesMR) + ' ' + randomizer.getRandomItem(randomizer.constants.familyNamesMR));
-            }
-        } else if (typeof args[1] !== 'undefined'&& args[1].toUpperCase().startsWith('H')) {
-            if (args[2].toUpperCase().startsWith('M')) {
-                message.channel.send(randomizer.getRandomItem(randomizer.constants.maleFirstNamesHR) + ' ' + randomizer.getRandomItem(randomizer.constants.familyNamesHR));
-            } else if (args[2].toUpperCase().startsWith('W')) {
-                message.channel.send(randomizer.getRandomItem(randomizer.constants.femaleFirstNamesHR) + ' ' + randomizer.getRandomItem(randomizer.constants.familyNamesHR));
-            }
-            message.channel.send(item + ' :rofl:');
-        } else {
-            const firstnames = randomizer.constants.maleFirstNamesMR.concat(randomizer.constants.maleFirstNamesHR).concat(randomizer.constants.femaleFirstNamesHR).concat(randomizer.constants.femaleFirstNamesMR);
-            const familynames = randomizer.constants.familyNamesHR.concat(randomizer.constants.familyNamesMR);
-            message.channel.send(randomizer.getRandomItem(firstnames) + ' ' + randomizer.getRandomItem(familynames));
-        }
+        getNames();
     } else {
         message.channel.send('Es existiert keine Zufallsliste für diesen Wert. Gib **$help** für eine Liste gültiger Eingaben ein.')
     }
+
+
+    function getNames() {
+        let culture;
+        if (typeof args[1] !== 'undefined') {
+
+            if (args[1].toUpperCase().startsWith('G')) {
+                culture = 'Gareth';
+            } else if (args[1].toUpperCase().startsWith('H')) {
+                culture = 'Horas';
+            } else if (args[1].toUpperCase().startsWith('A')) {
+                culture = 'Albernia';
+            } else if (args[1].toUpperCase().startsWith('W')) {
+                culture = 'Weiden';
+            } else if (args[1].toUpperCase().startsWith('T')) {
+                culture = 'Tulamidisch';
+            } else if (args[1].toUpperCase().startsWith('B')) {
+                culture = 'Bornland';
+            } else {
+                message.channel.send('Es gibt keine Kultur mit diesem Namen. Gib **$help** ein für eine Liste gültiger Eingaben.')
+            }
+        }
+        let gender;
+        if (typeof args[2] !== 'undefined') {
+            if (args[2].toUpperCase().startsWith('M')) {
+                gender = 'firstNameMale';
+            } else if (args[2].toUpperCase().startsWith('W')) {
+                gender = 'firstNameFemale';
+            } else {
+                message.channel.send('Es gibt kein Gender mit diesem Namen. Gib **$help** ein für eine Liste gültiger Eingaben.')
+            }
+        }
+        if (culture !== undefined && gender !== undefined) {
+            db.query('SELECT * FROM names WHERE culture = ' + '"' + culture + '"', function (err, row) {
+                if (row && err) {
+                    message.reply('Es gab einen Fehler.');
+                } else {
+                    if (gender === 'firstNameMale') {
+                        firstNames = row[0].firstNameMale.split(', ');
+                        lastNames = row[0].lastName.split(', ');
+                        message.channel.send(getRandomItem(firstNames) + ' ' + getRandomItem(lastNames));
+                    } else {
+                        firstNames = row[0].firstNameFemale.split(', ');
+                        lastNames = row[0].lastName.split(', ');
+                        message.channel.send(getRandomItem(firstNames) + ' ' + getRandomItem(lastNames));
+                    }
+                }
+            })
+        } else if (culture !== undefined) {
+            db.query('SELECT * FROM names WHERE culture = ' + '"' + culture + '"', function (err, row) {
+                if (row && err) {
+                    message.reply('Es gab einen Fehler.');
+                } else {
+                    firstNames = row[0].firstNameMale.split(', ').concat(row[0].firstNameFemale.split(', '));
+                    lastNames = row[0].lastName.split(', ');
+                    message.channel.send(getRandomItem(firstNames) + ' ' + getRandomItem(lastNames));
+                }
+            })
+        } else {
+            message.channel.send('Der Befehl ist zu unspezifisch. Gib **$help** ein für eine Liste gültiger Eingaben.');
+        }
+    }
 }
+
+function getRandomItem(items) {
+    return items[Math.floor(Math.random() * items.length)];
+}
+
